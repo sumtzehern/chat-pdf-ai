cfbf200086e59e705aa988a07031dfdc68f1c9b8
Wesley Sum <wesleysum0402@vt.edu>
2024-07-21 23:22:50 -0400

    v3 migrate

--- chatwithpdf/src/lib/db/s3-server.ts
+++ chatwithpdf/src/lib/db/s3-server.ts
@@ -1,30 +1,37 @@
-import AWS from 'aws-sdk';
-import fs from 'fs';
-
-export async function downloadFromS3(file_key: string) {
+import { S3 } from "@aws-sdk/client-s3";
+import fs from "fs";
+export async function downloadFromS3(file_key: string): Promise<string> {
+  return new Promise(async (resolve, reject) => {
     try {
-        AWS.config.update({
-            accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
-            secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY
-        });
+      const s3 = new S3({
+        region: process.env.NEXT_PUBLIC_S3_REGION!,
+        credentials: {
+          accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY!,
+        },
+      });
+      const params = {
+        Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
+        Key: file_key,
+      };
 
-        const s3 = new AWS.S3({
-            params: {
-                Bucket: process.env.NEXT_PUBLIC_AWS_BUCKET_NAME,
-            },
-            region: process.env.NEXT_PUBLIC_AWS_REGION
-        });
-        const params = {
-            Bucket: process.env.NEXT_PUBLIC_AWS_BUCKET_NAME!,
-            Key: file_key
-        };
-        const data = await s3.getObject(params).promise();
-        const file_name = `/tmp/pdf-${Date.now()}.pdf`;
-        fs.writeFileSync(file_name, data.Body as Buffer);
-        return file_name
+      const obj = await s3.getObject(params);
+      const file_name = `/tmp/${Date.now().toString()}.pdf`;
 
+      if (obj.Body instanceof require("stream").Readable) {
+
+        const file = fs.createWriteStream(file_name);
+        file.on("open", function (fd) {
+          // @ts-ignore
+          obj.Body?.pipe(file).on("finish", () => {
+            return resolve(file_name);
+          });
+        });
+      }
     } catch (error) {
-        console.log(error)
-        return null
+      console.error(error);
+      reject(error);
+      return null;
     }
+  });
 }
